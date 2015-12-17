25d01c46e493aa28ce844df2b91d9d9a8d77f5ab
Marcos Santos <marcos.junior44@fatec.sp.gov.br>
2015-12-17 16:35:53 -0200

    Initial Commit

---
+++ Pretty Apple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pretty Apple
+//
+//  Created by Marcos Santos on 12/17/15.
+//  Copyright © 2015 Marcos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
